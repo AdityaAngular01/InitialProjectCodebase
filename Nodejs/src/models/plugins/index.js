496f31295da0c62f7556b3aeb824cc3a50d57e8c
AdityaAngular01 <aditya@angularminds.in>
2025-07-18 11:00:26 +0530

    removed unuse depthPopulatePlugin and added auditTrailPlugin

--- Nodejs/src/models/plugins/index.js
+++ Nodejs/src/models/plugins/index.js
@@ -2,6 +2,5 @@ module.exports = {
     safeSoftDeletePlugin: require("./safeSoftDelete.plugin"),
     privateFieldsPlugin: require("./privateFields.plugin"),
     paginatePlugin: require("./paginate.plugin"),
-    depthPopulatePlugin: require("./depthPopulate.plugin"),
+    auditTrailPlugin: require("./auditTrail.plugin"),
 }
-    // auditTrailPlugin: require("./auditTrail.plugin"),
